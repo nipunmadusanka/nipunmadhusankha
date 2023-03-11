b7c68b4013d28982ef681c9fc5af80216217c54b
nipunmadusanka <nm3nipun@gmail.com>
2023-03-11 18:19:22 +0530

    newly

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Nipun Madhusankha</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/aos.css">
+     <link rel="stylesheet" href="css/owl.carousel.min.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/templatemo-digital-trend.css">
+
+</head>
+<body>
+
+     <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+              <i class="fa fas fa-user"></i>
+              Hi, I am Nipun Madusanka
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link smoothScroll">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#project" class="nav-link smoothScroll">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#Skills" class="nav-link smoothScroll">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#edu" class="nav-link smoothScroll">Education and Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#footer" class="nav-link smoothScroll">Contact me</a>
+                    </li>
+                    
+                    <!-- <li class="nav-item">
+                        <a href="blog.html" class="nav-link">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.html" class="nav-link contact">Contact</a>
+                    </li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+     <!-- HERO -->
+     <section class="hero hero-bg d-flex justify-content-center align-items-center">
+               <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
+                              <div class="hero-text">
+
+                                   <h1 class="text-white" data-aos="fade-up">If you want to make peace with your enemy, you have to work with your enemy. Then he becomes your partner.</h1>
+
+                                   <a href="#footer" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Contact me!</a>
+
+                                   
+                              </div>
+                        </div>
+
+                        <div class="col-lg-6 col-12">
+                          <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
+
+                            <img src="images/nipun1.jpg" class="img-fluid" style="border-radius: 50%; width: 96%; " alt="nipun">
+                          </div>
+                        </div>
+
+                    </div>
+               </div>
+     </section>
+
+
+     <!-- ABOUT -->
+     <section class="about section-padding pb-0" id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-7 mx-auto col-md-10 col-12">
+                         <div class="about-info">
+
+                              <h2 class="mb-4" data-aos="fade-up"><strong>About Me</strong></h2>
+
+                              <p class="mb-0" data-aos="fade-up">I am a designer, developer and lifelong student. I am an optimist. I am an environmentalist.
+You can also identify me as an advocate against injustice. I am a Sri Lankan who wants to see every part of the world. Gathering as much experience as possible in a lifetime is one of my hopes and makes reading a hobby. The most precious thing in my life is the freedom I get. I will always travel in search of freedom.</p>
+                         <!-- <a href="Nipun Madhushanka_CV.pdf" target="_blank" class="custom-btn btn-bg btn mt-3" data-aos="fade-up" data-aos-delay="100">Download CV</a> -->
+                         </div>
+
+                         <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+
+                          <img src="images/office.png" class="img-fluid" alt="office">
+                        </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+
+
+     <!-- PROJECT -->
+     <section class="project section-padding" id="project">
+          <div class="container-fluid">
+               <div class="row">
+
+                    <div class="col-lg-12 col-12">
+
+                        <h2 class="mb-5 text-center" data-aos="fade-up">
+                           <strong>Portfolio
+                            </strong>
+                        </h2>
+
+                         <div class="owl-carousel owl-theme" id="project-slide">
+                              <div class="item project-wrapper" data-aos="fade-up" data-aos-delay="100">
+                              <a href="images/project/miracle 1-8.png"  target="_blank">
+                                   <img src="images/project/miracle 1-8.png" class="img-fluid" alt="project image" style="width: ">
+                                   </a>
+
+                                   <div class="project-info">
+                                        <h3>
+                                        <a href="https://github.com/nipunmadusanka/miracle_creation-freelancer_project"  target="_blank">
+                                                  <span>Freelance web application</span>
+                                                  <i class="fa fa-angle-right project-icon"></i>
+                                             </a>
+                                        </h3>
+                                        <small>HTML/CSS, Bootstrap, JavaScript, PHP, MySQL</small>
+                                   </div>
+                              </div>
+
+                              <div class="item project-wrapper" data-aos="fade-up">
+                              <a href="images/project/medicine delevery system for rathnapura hospital.png"  target="_blank">
+                                   <img src="images/project/medicine delevery system for rathnapura hospital.png" class="img-fluid" alt="project image">
+                                   </a>
+
+                                   <div class="project-info">
+                                        <h3>
+                                             <a href="#"  target="_blank">
+                                                  <span>Medicine Delevery System</span>
+                                                  <i class="fa fa-angle-right project-icon"></i>
+                                             </a>
+                                        </h3>
+                                        <small>HTML/CSS, Bootstrap, JavaScript, PHP, MySQL</small>
+                                   </div>
+                              </div>
+                              <div class="item project-wrapper" data-aos="fade-up">
+                              <a href="images/project/Calculator with simple functions.png"  target="_blank">
+                                   <img src="images/project/Calculator with simple functions.png" class="img-fluid" alt="project image">
+                                   </a>
+
+                                   <div class="project-info">
+                                        <h3>
+                                        <a href="https://github.com/nipunmadusanka/simple-calculator"  target="_blank">
+                                                  <span>Simple Calculator</span>
+                                                  <i class="fa fa-angle-right project-icon"></i>
+                                             </a>
+                                        </h3>
+                                        <small>NetBeans ID, JAVA</small>
+                                   </div>
+                              </div>
+
+
+                              <div class="item project-wrapper" data-aos="fade-up">
+                              <a href="images/project/sale portal-7.png"  target="_blank">
+                                   <img src="images/project/sale portal-7.png" class="img-fluid" alt="project image">
+                                   </a>
+
+                                   <div class="project-info">                                       
+
+                                        <h3>
+                                        <a href="#"  target="_blank">
+                                                  <span>Online Shopping Portal</span>
+                                                  <i class="fa fa-angle-right project-icon"></i>
+                                             </a>
+                                        </h3>
+                                        <small>HTML/CSS, Bootstrap, JavaScript, PHP, MySQL</small>
+                                   </div>
+                              </div>
+
+                         
+                         </div>
+                         
+                    </div>
+                    <div class="col-lg-7 mx-auto col-md-10 col-12" id="Skills">
+                         <div class="about-info">
+
+                         <h2 class="mb-5 text-center" data-aos="fade-up">
+                           <strong>Skills</strong>
+                        </h2>
+                        <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+
+                          <img src="images/PROGRAMMING LANGUAGES.png" class="img-fluid" alt="office">
+                        </div>
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+
+     <!-- TESTIMONIAL -->
+     <section class="testimonial section-padding" id="edu">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-6 col-md-5 col-12">
+                        <div class="contact-image" data-aos="fade-up">
+
+                          <img src="images/work.gif" class="img-fluid" alt="my">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-7 col-12">
+                      <!-- <h4 class="my-5 pt-3" data-aos="fade-up" data-aos-delay="100">Education and experience</h4> -->
+                      <h2 class="mb-4" data-aos="fade-up"><strong>Education and Experience</strong></h2>
+
+                      <div class="quote" data-aos="fade-up" data-aos-delay="200"></div>
+
+                      <h2 class="mb-4" data-aos="fade-up" data-aos-delay="300">I am studying Software Technology at South Eastern University of Sri Lanka. currently working at Abacus Technology as a software engineer.<br></h2>
+
+                      <!-- <p data-aos="fade-up" data-aos-delay="400">
+                        <strong>nipun madhusankha </strong>
+
+                        <span class="mx-1">/</span>
+
+                        <small><a href="http://ideacloud.webnode.com/" target="_blank">IdeaCloud</a></small>
+                      </p> -->
+                    </div>
+
+               </div>
+          </div>
+     </section>
+
+
+    <footer class="site-footer" id="footer">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">Forward through <strong>creativity</strong></h1>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="200">
+            <h4 class="my-4">Contact Info</h4>
+
+            <p class="mb-1">
+            <a href="tel:+9476 949 2821">
+              <i class="fa fa-phone mr-2 footer-icon"></i> 
+              +9476 949 2821
+              </a>
+            </p>
+
+            <p>
+              <a href="#">
+                <i class="fa fa-envelope mr-2 footer-icon"></i>
+                jmnipun@outlook.com
+              </a>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 col-12" data-aos="fade-up" data-aos-delay="300">
+            <h4 class="my-4">Address</h4>
+
+            <p class="mb-1">
+              <i class="fa fa-home mr-2 footer-icon"></i> 
+              Buttala, Sri Lanka
+            </p>
+          </div>
+
+          <div class="col-lg-4 mx-lg-auto text-center col-md-8 col-12" data-aos="fade-up" data-aos-delay="400">
+            <p class="copyright-text">Copyright &copy; nipunmadhusankha
+            <br>
+          
+          </div>
+
+          <div class="col-lg-4 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="500">
+            
+            <ul class="footer-link">
+<!--               <li><a href="#">Stories</a></li>
+              <li><a href="#">Work with us</a></li>
+              <li><a href="#">Privacy</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-lg-3 mx-lg-auto col-md-6 col-12" data-aos="fade-up" data-aos-delay="600">
+            <ul class="social-icon">
+              <li><a href="https://www.instagram.com/jm_nipun/?fbclid=IwAR1gy4v9BBT_SaB4rQamiPqPlHif49AJ6-9R4f9lzs8Wkx5jJoMXt3UsEWw" class="fa fa-instagram" target="_blank"></a></li>
+              <li><a href="https://twitter.com/nm3nipun?fbclid=IwAR0eLITiwC67u0X8nJ1q3HQFlhXrfxymziggPxOMFWgJZd2WSE9t_6E1_xM" class="fa fa-twitter" target="_blank"></a></li>
+              <!-- <li><a href="https://www.facebook.com/profile.php?id=100008536545362" class="fa fa-facebook"></a></li> -->
+              <li><a href="https://www.behance.net/nipunmadusanka_lk" class="fa fa-behance" target="_blank"></a></li>
+              <li><a href="https://github.com/nipunmadusanka" class="fa fa-github" target="_blank"></a></li>
+              
+             
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.min.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/aos.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
